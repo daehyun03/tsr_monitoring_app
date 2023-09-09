37830deb31a3c32f82504dbed93fa73093dd23c1
daehyun03 <kimdeahyeon5445@gmail.com>
2023-09-09 19:07:23 +0900

    진공펌프 구현중

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,4 +1,5 @@
-const Map<String, String> machineNameMap = {"쇼트블라스트": "ShotBlast", "인산염피막기": "AROPump", "비형상도포기": "Dispenser", "진공펌프": "VacuumPump"};
+const Map<String, String> machineNameMap = {"쇼트블라스트": "ShotBlast", "인산염피막기": "AROPump", "비형상도포기": "Dispenser",
+  "진공펌프": "VacuumPump", "진공펌프1": "VacuumPump1","진공펌프2": "VacuumPump2"};
 const List machineList = ["쇼트블라스트", "인산염피막기", "비형상도포기", "진공펌프"];
 const SHOT_BLAST = "쇼트블라스트";
 const ARO_PUMP = "인산염피막기";
@@ -7,10 +8,8 @@ const VACUUM_PUMP = "진공펌프";
 //const BASE_URL = "http://202.31.202.34:443";
 const BASE_URL = "http://182.228.122.118:8445";
 const DATE_FORMAT = "yyyy-MM-dd";
-//const String BASE_URL = "http://192.168.0.8:8080";
 
 const ANOMALY_EVENT = "anomaly";
-
 
 //******* REQUEST URL *******//
 const SHOT_BLAST_URL = "/sio/ShotBlast";
@@ -60,5 +59,20 @@ List getChanelNameList(String name) {
       return VACUUM_PUMP_CHANNEL_NAME_ARRAY;
     default:
       return [];
+  }
+}
+
+bool getIsOneSocket(String name) {
+  switch(name) {
+    case "쇼트블라스트":
+      return true;
+    case "인산염피막기":
+      return true;
+    case "비형상도포기":
+      return true;
+    case "진공펌프":
+      return false;
+    default:
+      return false;
   }
 }
--- lib/widget/avg_chart.dart
+++ lib/widget/avg_chart.dart
@@ -12,16 +12,16 @@ class AvgDataChart extends StatefulWidget {
   AvgDataChart(this.machineName);
 
   @override
-  _AvgDataChart createState() => _AvgDataChart(machineName);
+  State<AvgDataChart> createState() => getIsOneSocket(machineName) ? _AvgDataChart1(machineName) : _AvgDataChart2(machineName);
 
 }
 
-class _AvgDataChart extends State<AvgDataChart> {
+class _AvgDataChart1 extends State<AvgDataChart> {
   late String machineName;
   List<List<_dateAvgData>> data = [];
   DateTime curDate = DateTime.now();
   DateTime today = DateTime.now();
-  _AvgDataChart(this.machineName);
+  _AvgDataChart1(this.machineName);
   late List channelList;
 
   @override
@@ -102,6 +102,7 @@ class _AvgDataChart extends State<AvgDataChart> {
           series: <ChartSeries<_dateAvgData, DateTime>>[
             for (int i = 0; i < channelList.length; i++)
               LineSeries<_dateAvgData, DateTime>(
+                  name: channelList[i],
                   dataSource: data[i],
                   xValueMapper: (_dateAvgData data, _) => data.date,
                   yValueMapper: (_dateAvgData data, _) => data.avg,
@@ -111,6 +112,109 @@ class _AvgDataChart extends State<AvgDataChart> {
       );
     }
   }
+}
+
+class _AvgDataChart2 extends State<AvgDataChart> {
+  late String machineName;
+  List<List<_dateAvgData>> data = [];
+  DateTime curDate = DateTime.now();
+  DateTime today = DateTime.now();
+  late List channelList;
+
+  _AvgDataChart2(this.machineName);
+
+  @override
+  void initState() {
+    super.initState();
+    channelList = getChanelNameList(machineName);
+
+    getDataList();
+  }
+
+  String getUrl(int i) {
+    String url = BASE_URL + "/stat/hour?machine=" + machineNameMap[machineName + i.toString()]! + "&date=" + DateFormat(DATE_FORMAT).format(curDate);
+    print(url);
+    return url;
+  }
+
+  void getDataList() async {
+    data = [];
+    for(int i = 0; i < channelList.length; i++) {
+      final url = Uri.parse(getUrl(i+1));
+      final res = await http.get(url);
+      List list = jsonDecode(res.body)[channelList[i] + "_hour_avg"] as List;
+      List<_dateAvgData> tmp = [];
+      for (int i = 0; i < list.length; i++) {
+        tmp.add(_dateAvgData(DateTime.parse(list[i][0]), list[i][1]));
+      }
+      data.add(tmp);
+    }
+
+    setState(() {
+      data = data;
+      print(data);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+        children: [
+          const Padding(padding: EdgeInsets.only(top: 20)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('일일 평균 조회     ', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 25),),
+              Container(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    textStyle: MaterialStateProperty.all<TextStyle>(TextStyle(fontSize: 20)),
+                  ),
+                  onPressed: () async{
+                    final selectedDate = await showDatePicker(context: context, initialDate: curDate, firstDate: DateTime(2023), lastDate: today,
+                        initialEntryMode: DatePickerEntryMode.calendarOnly);
+                    if (selectedDate != null) {
+                      setState(() {
+                        curDate = selectedDate;
+                        getDataList();
+                      });
+                    }
+                  },
+                  child: Text(DateFormat(DATE_FORMAT).format(curDate)),
+                ),
+              )
+            ],
+          ),
+          const Padding(padding: EdgeInsets.only(top: 20)),
+          Expanded(child: _buildChart())
+        ]
+    );
+  }
+
+  Widget _buildChart () {
+    if (data.isEmpty) {
+      return const Text("데이터가 없습니다.");
+    } else {
+      return SfCartesianChart(
+          tooltipBehavior: TooltipBehavior(enable: true),
+          primaryXAxis: DateTimeCategoryAxis(dateFormat: DateFormat("HH")),
+          primaryYAxis: NumericAxis(interval: 0.1),
+          series: <ChartSeries<_dateAvgData, DateTime>>[
+            for (int i = 0; i < channelList.length; i++)
+              LineSeries<_dateAvgData, DateTime>(
+                  name: channelList[i],
+                  dataSource: data[i],
+                  xValueMapper: (_dateAvgData data, _) => data.date,
+                  yValueMapper: (_dateAvgData data, _) => data.avg,
+                  dataLabelSettings: DataLabelSettings(isVisible: true)
+              ),
+          ]
+      );
+    }
+  }
+
+
 }
 
 class _dateAvgData {
